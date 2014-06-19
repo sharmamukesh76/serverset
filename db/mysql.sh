434d0f0d56e721550253e7047572993e5db0d6f5
Viktor Farcic <viktor.farcic@everis.com>
2014-06-19 18:49:43 +0200

    Added JIRA

--- db/mysql.sh
+++ db/mysql.sh
@@ -10,6 +10,15 @@ service mysqld start
 
 # DBs
 mysql -e "CREATE user 'root';"
+mysql -e "CREATE user 'alm';"
+mysql -e "CREATE DATABASE jira CHARACTER SET utf8 COLLATE utf8_bin;"
+mysql -e "GRANT ALL PRIVILEGES ON jira.* TO 'alm'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON jira.* TO 'alm'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON jira.* TO 'alm'@'localhost' IDENTIFIED BY 'mdp@2014';"
+mysql -e "GRANT ALL PRIVILEGES ON jira.* TO 'root'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON jira.* TO 'root'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON jira.* TO 'root'@'localhost' IDENTIFIED BY 'mdp@2014';"
+mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE DATABASE stash CHARACTER SET utf8 COLLATE utf8_bin;"
 mysql -e "GRANT ALL PRIVILEGES ON stash.* TO 'root'@'%';"
 mysql -e "GRANT ALL PRIVILEGES ON stash.* TO 'root'@'localhost';"
